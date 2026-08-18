8e9e647b3275673f7d4547cf3dd0c88f65440090
Gustavo <gustavonori558@gmail.com>
2026-08-17 22:03:18 -0300

    Comeco de reformulação do banco

--- src/entities/Notice.js
+++ src/entities/Notice.js
@@ -2,65 +2,66 @@ import { EntitySchema } from "typeorm";
 
 export const NoticeEntity = new EntitySchema({
   name: "Notice",
+
   tableName: "notices",
+
   columns: {
     id: {
       primary: true,
       type: "int",
       generated: true,
     },
+
     title: {
       type: "varchar",
+      nullable: false,
     },
+
+    state: {
+      type: "varchar",
+      nullable: true,
+    },
+
+    state_code: {
+      type: "varchar",
+      nullable: true,
+    },
+
     description: {
       type: "text",
       nullable: true,
     },
-    publication_date: {
-      type: "date",
-      nullable: true,
-    },
+
     link: {
       type: "varchar",
       unique: true,
       nullable: false,
     },
+
+    publication_date: {
+      type: "date",
+      nullable: true,
+    },
+
     created_at: {
       type: "timestamp",
       createDate: true,
     },
-    state_id: {
-      type: "int",
-      nullable: true,
-    },
-    area_id: {
-      type: "int",
-      nullable: true,
-    },
   },
+
   relations: {
-    state: {
-      type: "many-to-one",
-      target: "State",
-      joinColumn: { name: "state_id" },
-      nullable: true,
-    },
-    area: {
-      type: "many-to-one",
-      target: "Area",
-      joinColumn: { name: "area_id" },
-      nullable: true,
-    },
     simulations: {
       type: "one-to-many",
       target: "Simulation",
       inverseSide: "notice",
     },
+
     email_logs: {
       type: "one-to-many",
       target: "EmailLog",
       inverseSide: "notice",
     },
+
     favorites: {
       type: "one-to-many",
       target: "Favorite",
