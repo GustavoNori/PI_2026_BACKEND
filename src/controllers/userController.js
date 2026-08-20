22154aea3d5d9e87a1d0543ca1ef8d52fc625194
Gustavo <gustavonori558@gmail.com>
2026-08-20 19:53:25 -0300

    feat: Desativação de usuario Premium

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -254,4 +254,30 @@ export class AuthController {
             return res.status(500).json({ message: "Internal server error" });
         }
     }
+
+    async deactivatePremium(req, res) {
+        try {
+            const repo = AppDataSource.getRepository(SubscriptionEntity);
+            const { id } = req.params;
+
+            const subscription = await repo.findOne({
+                where: {
+                    user_id: parseInt(id),
+                    status: "active"
+                }
+            });
+
+            if (!subscription) {
+                return res.status(404).json({ message: "Subscription not found" });
+            }
+            else {
+                subscription.status = "inactive";
+                await repo.save(subscription);
+                return res.status(200).json({ message: "Premium desativado com sucesso" });
+            }
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({ message: "Internal server error" });
+        }
+    }
 }
