faa24b7f2c8dcc685b4f212a3dd50d6be4c49232
Raiane Cecílio <cecilioraiane371@gmail.com>
2026-09-10 07:58:24 -0300

    add rote premium

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -10,15 +10,15 @@ router.post("/login", authController.login);
 router.post("/users", authController.createUser);
 
 router.get("/users", authMiddleware, checkRole("admin"), authController.getAllUsers);
-router.get("/users/:id", authMiddleware, checkRole("admin", "user"), authController.getOneUser);
-router.put("/users/:id", authMiddleware, checkRole("admin", "user"), authController.updateUser);
+router.get("/users/:id", authMiddleware, checkRole("admin", "user", "premium"), authController.getOneUser);
+router.put("/users/:id", authMiddleware, checkRole("admin", "user", "premium"), authController.updateUser);
 router.delete("/users/:id", authMiddleware, checkRole("admin"), authController.deleteUser);
 
 router.patch("/users/:id/promote", authMiddleware, checkRole("admin"), authController.promoteToAdmin);
 router.patch("/users/:id/demote", authMiddleware, checkRole("admin"), authController.demoteToUser);
 
-router.get("/users/:id/subscription", authMiddleware, checkRole("admin", "user"), authController.getUserSubscription);
-router.post("/users/:id/activate-premium", authMiddleware, checkRole("admin", "user"), authController.activatePremium);
+router.get("/users/:id/subscription", authMiddleware, checkRole("admin", "user", "premium"), authController.getUserSubscription);
+router.post("/users/:id/activate-premium", authMiddleware, checkRole("admin", "user", "premium"), authController.activatePremium);
 
 
 router.post("/auth/forgot-password", authController.forgotPassword);
